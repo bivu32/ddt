00df1783e4b55f1be522ba16f79d004e41c45a16
Deepanway Ghosal <deepanwayedu@gmail.com>
2019-05-14 16:55:58 +0800

    Update loadDailyDialog.py

--- loadDailyDialog.py
+++ loadDailyDialog.py
@@ -91,7 +91,7 @@ dataset = data.TabularDataset('ddt/dailydialog.csv',
                             )
 label.build_vocab(dataset)
 
-vectors = vocab.Vectors(name='glove.6B.100d.txt', cache='content/drive/bivu/')
+vectors = vocab.Vectors(name='glove.840B.300d.txt', cache='content/drive/')
 
 id.build_vocab(dataset)
 label.build_vocab(dataset)
